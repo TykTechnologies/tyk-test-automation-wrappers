84508b9e5f7268e2967a4e7d1ede84b4e55f7433
konrad <konrad@tyk.io>
2023-06-15 18:03:01 +0200

    update with build

---
+++ wrappers/index.ts
@@ -0,0 +1,4 @@
+export { Button_object } from './Button_object';
+export { DropDown_object } from './DropDown_object';
+export { Input_object } from './Input_object';
+export { Wrapper } from './Wrapper';
